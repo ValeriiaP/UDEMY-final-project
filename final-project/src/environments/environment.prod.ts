d4dfed42de831b8ead266f28fd45bec7241f08a7
Valeriia Parfeliuk <parfelyuk.v@gmail.com>
2017-10-10 11:43:31 +0300

    Add new project, include firebase and bootstrap.

---
+++ final-project/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAM-LpwCR-s1avSxsZGVP8iO6HswcF-9Us",
+    authDomain: "organic-shop-9011a.firebaseapp.com",
+    databaseURL: "https://organic-shop-9011a.firebaseio.com",
+    projectId: "organic-shop-9011a",
+    storageBucket: "organic-shop-9011a.appspot.com",
+    messagingSenderId: "465793221743"
+  }
+};
