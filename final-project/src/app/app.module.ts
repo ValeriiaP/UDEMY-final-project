0addbf9e7609353eaf69168b23de6a8ecff762df
Valeriia Parfeliuk <parfelyuk.v@gmail.com>
2017-10-23 12:07:08 +0300

    Added protecting routes

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {AuthService} from "./auth.service";
+import {AuthGuard} from "./auth-guard.service";
 
 
 @NgModule({
@@ -44,16 +45,21 @@ import {AuthService} from "./auth.service";
       { path: '', component: HomeComponent},
       { path: 'products', component: ProductsComponent},
       { path: 'shopping-cart', component: ShoppingCartComponent},
-      { path: 'check-out', component: CheckOutComponent},
-      { path: 'order-success', component: OrderSuccessComponent},
       { path: 'login', component: LoginComponent},
-      { path: 'admin/products', component: AdminProductsComponent},
-      { path: 'admin/orders', component: AdminOrdersComponent},
-      { path: 'my/orders', component: MyOrdersComponent}
+
+      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard]},
+      { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard]},
+      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]},
+      
+      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard]},
+      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard]}
     ]),
     NgbModule.forRoot()
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
